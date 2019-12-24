d151362d4584c4293417101d199b6a2a03ba151b
foestauf <46908845+foestauf@users.noreply.github.com>
2019-12-24 16:16:16 -0500

    Fixed projects, added colums, rows, container.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import '../assets/sass/Projects.scss';
+// import '../assets/sass/bootstrap/bootstrap-grid.css';
 
 class Projects extends Component {
     constructor(props) {
@@ -9,10 +10,29 @@ class Projects extends Component {
         };
     }
     render() {
-        return {
-
-        }
-    };
+        return (
+            <div>
+              <div class="container">
+                  <div class="row">
+                      <div class="col-3">Col 1 of 2</div>
+                      <div class="col-6">Col 2 of 2</div>
+                  </div>
+                  <div class="row">
+                      <div class="col">Col 1 of 2</div>
+                      <div class="col">Col 2 of 2</div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md">
+                        <p>Hello World</p>
+                    </div>
+                    <div class="col">
+                        <p>Col 2 of 2 hello</p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        )
+    }
 
 }
 
