1e2c5698f58f88612be825760980fe58067a772f
foestauf <46908845+foestauf@users.noreply.github.com>
2020-01-08 20:27:25 -0500

    Styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -71,43 +71,49 @@ class Projects extends Component {
 
     render() {
         return (
-            <div>
-                <div className="highlight">
-                    <div className="col-3"><Highlight value={this.state.value}/></div>
-                    <div className="col-6"><Listing value={this.state.value}/></div>
-                </div>
-                <div className="container">
-                  <div className="row">
-                      <div className="col">
+          <section
+            className="resume-section p-3 p-lg-5 d-flex justify-content-center"
+            id="experience">
+            <div className="w-100">
+              <h2 className="mb-5">Experience</h2>
+                <div className="container d-flex flex-wrap">
+                  <div className="row no-gutters">
+                      <div className="col px-4 m-2">
                         <button onClick={() => this.setState({value: 1})}>
                             <img className="langLogo" src={lang1Logo} alt="R Logo"/>
                         </button>
                       </div>
-                      <div className="col">
+                      <div className="col px-4 m-2">
                         <button onClick={() => this.setState({value: 2})}>
                             <img className="langLogo" src={lang2Logo} alt="Python Logo"/>
                         </button>
                         </div>
+                    <div className="col px-4 m-2">
+                      <button onClick={() => this.setState({value: 3})}>
+                        <img className="langLogo" src={lang3Logo} alt="mySQL Logo"/>
+                      </button>
+                    </div>
+                    <div className="col px-4 m-2">
+                      <button onClick={() => this.setState({value: 4})}>
+                        <img className="langLogo" src={lang4Logo} alt="SAS Logo"/>
+                      </button>
+                    </div>
+                    <div className="col px-4 m-2 ">
+                      <button onClick={() => this.setState({ value: 5 })}>
+                        <img className="langLogo" src={lang5Logo} alt="Tableau Logo"/>
+                      </button>
+                    </div>
                   </div>
-                  <div className="row">
-                    <div className="col">
-                        <button onClick={() => this.setState({value: 3})}>
-                            <img className="langLogo" src={lang3Logo} alt="mySQL Logo"/>
-                        </button>
-                    </div>
-                    <div className="col">
-                        <button onClick={() => this.setState({value: 4})}>
-                            <img className="langLogo" src={lang4Logo} alt="SAS Logo"/>
-                        </button>
-                    </div>
-                        <div className="col">
-                            <button onClick={() => this.setState({ value: 5 })}>
-                                <img className="langLogo" src={lang5Logo} alt="Tableau Logo"/>
-                            </button>
-                        </div>
+                  <div className="row no-gutters">
+
                   </div>
               </div>
+              <div className="highlight resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div className="col-3"><Highlight value={this.state.value}/></div>
+                <div className="col-6"><Listing value={this.state.value}/></div>
+              </div>
             </div>
+          </section>
         )
     }
 
